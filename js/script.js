0edfd5194f08ac7957611cd1579da96f9a100f38
Robert Smalarz <smalarz.robert@gmail.com>
2018-07-13 18:05:51 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -1,21 +1,8 @@
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'left',
-  contain: true
-});
-
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-  // options
-});
-
 // vanilla JS
 var flkty = new Flickity( '.carousel', {
   hash: true,
+  cellAlign: 'left',
 });
-
 
 var progressBar = document.querySelector('.progress-bar')
 
