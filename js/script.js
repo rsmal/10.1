9bc244b8bcf56b725188f09bcb44053273046e76
Robert Smalarz <smalarz.robert@gmail.com>
2018-07-15 12:07:39 +0200

    mustache,js

--- js/script.js
+++ js/script.js
@@ -23,3 +23,40 @@ buttonGroup.addEventListener( 'click', function( event ) {
   var index = buttons.indexOf( event.target );
   flkty.select( index );
 });
+
+
+
+
+
+
+var templateList = document.getElementById('template-carousel-list').innerHTML;
+var templateItem = document.getElementById('template-carousel-item').innerHTML;
+	
+	// Następnie zoptymalizujemy drugą z nich, ponieważ tylko ona będzie wykonywana wielokrotnie. 	
+
+	Mustache.parse(templateItem);
+	
+	// Teraz stworzymy zmienną, w której chcemy mieć kod HTML wszystkich produktów. 
+	
+	var listItems = '';
+	
+	/* Czas napisać pętlę, która dla każdego elementu z listy:
+	1. wygeneruje kod HTML dla danego produktu, oraz
+	2. doda ten wygenrowany kod HTML do zmiennej listItems. 
+	
+	Uwaga - zmienna productsData, której używamy poniżej, została zdefiniowana w kodzie HTML!
+	*/
+	
+	for(var i = 0; i < productsData.length; i++){
+		console.log(productsData);
+		listItems += Mustache.render(templateItem, productsData[i]);
+	}
+	
+	// Po wykonaniu pętli, zmienna listItems zawiera już kod HTML dla wszystkich produktów. Teraz wykorzystamy szablon templateList, aby wstawić produkty do wrappera listy. 
+	
+	var fullProductList = Mustache.render(templateList, {products: listItems});
+	
+	// I w pełni wyrenderowaną listę wyświetlimy na stronie: 
+	
+	results.insertAdjacentHTML('beforeend', fullProductList);
+
