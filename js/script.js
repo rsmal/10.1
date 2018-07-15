a4b1e0cf4c78b08eb6c0aecb36e8b3cc6f8f196f
Robert Smalarz <smalarz.robert@gmail.com>
2018-07-15 15:29:56 +0200

    carousel problem

--- js/script.js
+++ js/script.js
@@ -4,12 +4,6 @@ var flkty = new Flickity( '.carousel', {
   cellAlign: 'left',
 });
 
-var flkty = new Flickity( '.carousel2', {
-  hash: true,
-  cellAlign: 'left',
-});
-
-
 var progressBar = document.querySelector('.progress-bar')
 
 flkty.on( 'scroll', function( progress ) {
@@ -31,36 +25,16 @@ buttonGroup.addEventListener( 'click', function( event ) {
 });
 
 
-
-
-	var templateList = document.getElementById('template-carousel-list').innerHTML;
-	var templateItem = document.getElementById('template-carousel-item').innerHTML;
-	
-	// Następnie zoptymalizujemy drugą z nich, ponieważ tylko ona będzie wykonywana wielokrotnie. 	
-
-	Mustache.parse(templateItem);
-	
-	// Teraz stworzymy zmienną, w której chcemy mieć kod HTML wszystkich produktów. 
-	
-	var listItems = '';
-	
-	/* Czas napisać pętlę, która dla każdego elementu z listy:
-	1. wygeneruje kod HTML dla danego produktu, oraz
-	2. doda ten wygenrowany kod HTML do zmiennej listItems. 
-	
-	Uwaga - zmienna productsData, której używamy poniżej, została zdefiniowana w kodzie HTML!
-	*/
-	
-	for(var i = 0; i < productsData.length; i++){
-		console.log(productsData);
-		listItems += Mustache.render(templateItem, productsData[i]);
-	}
-	
-	// Po wykonaniu pętli, zmienna listItems zawiera już kod HTML dla wszystkich produktów. Teraz wykorzystamy szablon templateList, aby wstawić produkty do wrappera listy. 
-	
-	var fullProductList = Mustache.render(templateList, {products: listItems});
-	
-	// I w pełni wyrenderowaną listę wyświetlimy na stronie: 
-	
-	results.insertAdjacentHTML('beforeend', fullProductList);
-	
+var templateList = document.getElementById('template-list').innerHTML;
+var templateItem = document.getElementById('template-slide').innerHTML;
+    Mustache.parse(templateItem);
+ 
+    var listItems = '';
+ 
+    for (var i = 0; i < carouselData.length; i++) {
+        console.log(carouselData);
+        listItems += Mustache.render(templateItem, carouselData[i]);
+    }
+ 
+    var fullProductList = Mustache.render(templateList, {carousel: listItems });
+    result.insertAdjacentHTML('beforeend', fullProductList);
