541ccbb13cec942a365f9388cf2e7333f607fe5f
Robert Smalarz <smalarz.robert@gmail.com>
2018-07-17 16:45:23 +0200

    task 10.2 fixed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
+'use strict';
+
+var carouselData = [{
+              text: 'Lorem ipsum ',
+              image: 'https://tinyurl.com/ydfh9r2x',
+              id: 'carousel-cell1',
+              description: ' <p>Lorem ipsum.</p> <img src="http://via.placeholder.com/400x200" alt="Photo placeholder"> <p>Dolor sit.</p>'
+          }, {
+              text: 'Lorem ipsum 1',
+              image: 'https://tinyurl.com/ycpsddow',
+              id: 'carousel-cell2',
+              description: ' <p>Lorem ipsum.</p> <img src="http://via.placeholder.com/400x200" alt="Photo placeholder"> <p>Dolor sit.</p>'
+          }, {
+              text: 'Lorem ipsum 2',
+              image: 'https://tinyurl.com/y8fahhnp',
+              id: 'carousel-cell3',
+              description: '<p>Lorem ipsum.</p> <img src="http://via.placeholder.com/400x200" alt="Photo placeholder"> <p>Dolor sit.</p> '
+          }, {
+              text: 'Lorem ipsum 3',
+              image: 'https://tinyurl.com/y8fahhnp',
+              id: 'carousel-cell4',
+              description: ' <p>Lorem ipsum.</p> <img src="http://via.placeholder.com/400x200" alt="Photo placeholder"> <p>Dolor sit.</p>'
+          }, {
+              text: 'Lorem ipsum 4',
+              image: 'https://tinyurl.com/ydfh9r2x',
+              id: 'carousel-cell5',
+              description: ' <p>Lorem ipsum.</p> <img src="http://via.placeholder.com/400x200" alt="Photo placeholder"> <p>Dolor sit.</p>'
+          }];
+
+
 
 var templateList = document.getElementById('template-list').innerHTML;
 var templateItem = document.getElementById('template-slide').innerHTML;
@@ -20,10 +50,10 @@ var templateItem = document.getElementById('template-slide').innerHTML;
 // vanilla JS
 var flkty = new Flickity( '.carousel', {
   hash: true,
-  cellAlign: 'left',
+  cellAlign: 'left'
 });
 
-var progressBar = document.querySelector('.progress-bar')
+var progressBar = document.querySelector('.progress-bar');
 
 flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
@@ -42,5 +72,3 @@ buttonGroup.addEventListener( 'click', function( event ) {
   var index = buttons.indexOf( event.target );
   flkty.select( index );
 });
-
-
